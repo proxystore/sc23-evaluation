0aa401f7b48dda3e8f813fd9b36a922ac5504001
valhayot <valeriehayot@gmail.com>
2023-06-25 20:19:09 -0500

    Add setup configuration and execution code for running DIM experiments on Chameleon

---
+++ dim-instructions/run_experiments.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+rep=5
+bench=../sc23-proxystore-analysis/data/1-proxystore-with-faas-rdma/chameleon-noop.csv
+globus_compute_inputs="1 10 100 1000 10000 100000"
+input_sizes="1 10 100 1000 10000 100000 1000000 10000000 100000000 1000000000"
+ep="5982cb6a-6ae7-44c0-8d9e-04899eacb934"
+HOST=$(hostname -I | awk -F ' ' '{print $NF}')
+
+exp=$1 # 0 = all, 1 = zmq, 2 = margo, 3 = redis, 4 = dspaces, 5 = globus_compute
+echo $exp
+
+if [ $exp == 1 ] || [ $exp == 0 ]
+then
+        python -m psbench.benchmarks.globus_compute_tasks --globus-compute-endpoint $ep --output-sizes 0 --input-sizes ${input_sizes} --ps-backend ZMQ --ps-host ${HOST} --ps-port 60000 --task-repeat $rep --csv-file $bench
+fi
+
+if [ $exp == 2 ] || [ $exp == 0 ]
+then
+        python -m psbench.benchmarks.globus_compute_tasks --globus-compute-endpoint $ep --output-sizes 0 --input-sizes ${input_sizes} --ps-backend MARGO --ps-port 55000 --ps-margo-protocol verbs --task-repeat $rep --csv-file $bench
+fi
+
+
+if [ $exp == 3 ] || [ $exp == 0 ]
+then
+        python -m psbench.benchmarks.globus_compute_tasks --globus-compute-endpoint $ep --output-sizes 0 --input-sizes ${input_sizes} --ps-backend REDIS --ps-host ${HOST} --ps-port 6379 --task-repeat $rep --csv-file $bench
+fi
+
+
+if [ $exp == 4 ] || [ $exp == 0 ]
+then
+        pkill -f dspaces_server
+        dspaces_server verbs &
+        python -m psbench.benchmarks.globus_compute_tasks --globus-compute-endpoint $ep --output-sizes 0 --input-sizes ${input_sizes} --dspaces --task-repeat $rep --csv-file $bench
+        pkill -f dspaces_server
+fi
+
+if [ $exp == 5 ] || [ $exp == 0 ]
+then
+        python -m psbench.benchmarks.globus_compute_tasks --globus-compute-endpoint $ep --output-sizes 0 --input-sizes ${globus_compute_inputs}  --task-repeat $rep --csv-file $bench
+fi
+
+if [ $exp == 6 ] || [ $exp == 0 ]
+then
+        python -m psbench.benchmarks.globus_compute_tasks --globus-compute-endpoint $ep --output-sizes 0 --input-sizes ${input_sizes} --ps-backend UCX --ps-port 60001 --task-repeat $rep --ps-interface ibp3s0 --csv-file $bench
+fi
---
+++ dim-instructions/setup-chameleon.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get update -y
+sudo apt-get install -y --no-install-recommends \
+  make gcc g++ cmake git build-essential ca-certificates autoconf automake libtool \
+  libjson-c-dev libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev pkg-config \
+  libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl libjson-c4 \
+  libjson-c-dev uuid numactl libnuma-dev \
+  libudev-dev libnl-3-dev libnl-route-3-dev ninja-build valgrind python3-dev cython3 \
+  python3-docutils pandoc
+
+sudo apt-get clean autoclean
+sudo rm -rf /var/lib/apt/lists/*
+
+# Get analysis and benchmark repo
+git clone -b dim-instructions https://github.com/proxystore/sc23-proxystore-analysis.git
+git clone -b dspaces https://github.com/proxystore/benchmarks.git
+
+mkdir downloads
+cd downloads
+
+# install mellanox ib drivers
+wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-4.9-6.0.6.0/MLNX_OFED_LINUX-4.9-6.0.6.0-ubuntu20.04-x86_64.tgz
+tar -xvf MLNX_OFED_LINUX-4.9-6.0.6.0-ubuntu20.04-x86_64.tgz
+cd MLNX_OFED_LINUX-4.9-6.0.6.0-ubuntu20.04-x86_64
+sudo ./mlnxofedinstall
+sudo /etc/init.d/openibd restart
+
+#### Configure static IP for infiniband  ####
+# Step 1: Check that interface is up and get interface name by using `ibv_devices` and `ip addr`
+# Step 2: create file /etc/netplan/01-netcfg.yaml
+# Step 3: Edit file with correct interface name and IP address that's within the same shared VLAN subnet (check on Chameleon what is the subnet and change Gateway4 if different) , e.g.,
+# network:
+#  version: 2
+#  renderer: networkd
+#  ethernets:
+#    ibp3s0:
+#      dhcp4: no
+#      addresses:
+#        - 10.52.3.144/24
+#      gateway4:  10.52.0.0
+#      nameservers:
+#          addresses: [8.8.8.8, 1.1.1.1]
+# Step 4: Update the network interfaces by running command `sudo netplan apply`
+# Step 5: Verify that interface has been updated by running `ip addr show <interface>`
+
+x=$(hostname -I |  awk -F '.' '{print $--NF}')
+y=$(hostname -I | awk -F '.' '{print $NF}')
+sudo bash -c "cat >> /etc/netplan/01-netcfg.yaml" << EOL
+network:
+ version: 2
+ renderer: networkd
+ ethernets:
+   ibp3s0:
+     dhcp4: no
+     addresses:
+       - 172.16.${x}.${y}/24
+     gateway4:  10.52.0.0
+     nameservers:
+         addresses: [8.8.8.8, 1.1.1.1]
+EOL
+
+sudo netplan apply
+
+# Install Spack
+git clone -c feature.manyFiles=true https://github.com/spack/spack.git
+source spack/share/spack/setup-env.sh
+echo 'source spack/share/spack/setup-env.sh' >> ~/.bashrc
+
+# Install Mochi packages
+git clone https://github.com/mochi-hpc/mochi-spack-packages.git
+
+# Install DataSpaces packages
+git clone git clone https://github.com/rdi2dspaces/dspaces-spack.git
+
+spack env create proxystore ~/sc23-proxystore-analysis/dim-instructions/spack-chameleon.yml
+spack env activate proxystore -p
+spack install
+
+echo 'spack env activate proxystore -p' >> ~/.bashrc
+pip install --upgrade pip
+
+## install UCX
+export C_INCLUDE_PATH=${C_INCLUDE_PATH}:$(find /home/cc/downloads/spack -regex '.*ucx.*/include')
+pip install git+https://github.com/rapidsai/ucx-py.git@v0.30.00
+
+## setup redis
+cd ~/benchmarks
+redis-server ~/sc23-proxystore-analysis/dim-instructions/redis.conf --daemonize yes
+
+## setup benchmarks and execute
+cd ~/benchmarks
+pip install -e .
+pip install globus_compute_sdk
+bash ~/sc23-proxystore-analysis/dim-instructions/run_experiments.sh 0
